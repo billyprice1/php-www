2c093ddbfbbba3d9e7e06005f8deafadeeb20fa2
Thomas <thomas.morain@olympe.in>
2014-06-23 17:58:01 +0200

    Bugfix

--- on/panel/messages/del_action.php
+++ on/panel/messages/del_action.php
@@ -11,6 +11,6 @@ api::send('self/message/del', array('id'=>$_GET['id']));
 if( isset($_GET['redirect']) )
 	template::redirect($_GET['redirect']);
 else
-	template::redirect('/panel/messages/detail?id='.security::encode($_GET['parent']));
+	template::redirect('/panel/messages');
 
 ?>
