9d38f3814a8f46ee23d2ddcce7f9e24d099d8401
Thomas <thomas.morain@olympe.in>
2014-08-23 23:15:25 +0200

    Reorganization and translation of documentation

--- on/doc/index.php
+++ on/doc/index.php
@@ -31,10 +31,10 @@ $content = "
 						<div style=\" padding-left: 10px; margin: 0 0 30px 0;\">
 							<h3 class=\"grey bordered\">{$lang['started']}</h3>
 							<ol>
-								<li><a href=\"/doc/what\">{$lang['what']}</a></li>
-								<li><a href=\"/doc/techno\">{$lang['techno']}</a></li>
-								<li><a href=\"/doc/info\">{$lang['infos']}</a></li>
 								<li><a href=\"/doc/first\">{$lang['first']}</a></li>
+								<li><a href=\"/doc/publish\">{$lang['publish']}</a></li>
+								<li><a href=\"/doc/info\">{$lang['infos']}</a></li>
+								<li><a href=\"/doc/php\">{$lang['php']}</a></li>
 							</ol>
 						</div>
 						<div style=\"padding-left: 10px; margin: 20px 0 30px 0;\">
@@ -51,19 +51,19 @@ $content = "
 						<div style=\"padding-left: 10px; margin: 0 0 30px 0;\">
 							<h3 class=\"grey bordered\">{$lang['features']}</h3>
 							<ol>
-								<li><a href=\"/doc/sites\">{$lang['sites']}</a></li>
 								<li><a href=\"/doc/domains\">{$lang['domains']}</a></li>
 								<li><a href=\"/doc/databases\">{$lang['databases']}</a></li>
-								<li><a href=\"/doc/mails\">{$lang['mails']}</a></li>	
+								<li><a href=\"/doc/mails\">{$lang['mails']}</a></li>
+								<li><a href=\"/doc/backups\">{$lang['backups']}</a></li>	
 							</ol>	
 						</div>
 						<div style=\"padding-left: 10px; margin: 20px 0 30px 0;\">
-							<h3 class=\"grey bordered\">{$lang['various']}</h3>
+							<h3 class=\"grey bordered\">{$lang['about']}</h3>
 							<ol>
-								<li><a href=\"/doc/faq\">{$lang['faq']}</a></li>
-								<li><a href=\"/doc/directory\">{$lang['directory']}</a></li>
-								<li><a href=\"/doc/backups\">{$lang['backups']}</a></li>	
-								<li><a href=\"/doc/php\">{$lang['php']}</a></li>
+								<li><a href=\"/doc/what\">{$lang['what']}</a></li>
+								<li><a href=\"/doc/techno\">{$lang['techno']}</a></li>
+								<li><a href=\"/about/contact\">{$lang['contact']}</a></li>
+								<li><a href=\"/about/contact?report\">{$lang['report']}</a></li>
 							</ol>			
 						</div>						
 					</div>
@@ -75,7 +75,7 @@ $content = "
 						<div class=\"faq\">
 							<div id=\"questions\">
 								<div style=\"float: left; width: 400px;\">
-									<div id=\"question-1\" class=\"question\" onclick=\"showAnswer(1); return false;\">{$lang['question_1']}</div>
+									<div id=\"question-1\" class=\"question\"><a style=\"color:#e89062;\" href=\"/doc/publish\">{$lang['question_1']}</a></div>
 									<div id=\"question-2\" class=\"question\" onclick=\"showAnswer(2); return false;\">{$lang['question_2']}</div>
 									<div id=\"question-3\" class=\"question\" onclick=\"showAnswer(3); return false;\">{$lang['question_3']}</div>
 									<div id=\"question-4\" class=\"question\" onclick=\"showAnswer(4); return false;\">{$lang['question_4']}</div>
@@ -83,37 +83,11 @@ $content = "
 								</div>
 								<div style=\"float: right; width: 400px;\">
 									<div id=\"question-6\" class=\"question\" onclick=\"showAnswer(6); return false;\">{$lang['question_6']}</div>
-									<div id=\"question-7\" class=\"question\" onclick=\"showAnswer(7); return false;\">{$lang['question_7']}</div>
+									<div id=\"question-7\" class=\"question\"><a style=\"color:#e89062;\" href=\"/doc/stats\">{$lang['question_7']}</a></div>
 									<div id=\"question-8\" class=\"question\" onclick=\"showAnswer(8); return false;\">{$lang['question_8']}</div>
 									<div id=\"question-9\" class=\"question\" onclick=\"showAnswer(9); return false;\">{$lang['question_9']}</div>
 									<div id=\"question-10\" class=\"question\" onclick=\"showAnswer(10); return false;\">{$lang['question_10']}</div>
 								</div>
-							</div>
-							<div id=\"answer-1\" class=\"answer\">
-								<div style=\"position: absolute; right: 0; top: 0;\"><a href=\"#\" onclick=\"showQuestions(1); return false;\"><img class=\"link\" src=\"/{$GLOBALS['CONFIG']['SITE']}/images/icons/small/close.png\" alt=\"\" /></a></div>
-								<p>{$lang['transfer']}</p>
-								<br />
-								<table>
-									<tr>
-										<th>{$lang['type']}</th>
-										<th>{$lang['hostname']}</th>
-										<th>{$lang['port']}</th>
-										<th>{$lang['user']}</th>
-									</tr>
-									<tr>	
-										<td>FTP</td>
-										<td>ftp.olympe.in</td>
-										<td>21</td>
-										<td><i>{$lang['name']}</i></td>
-									</tr>
-									<tr>	
-										<td>SFTP</td>
-										<td>ftp.olympe.in</td>
-										<td>22</td>
-										<td><i>{$lang['name']}</i></td>
-									</tr>
-								</table>
-								<br /><br />
 							</div>
 							<div id=\"answer-2\" class=\"answer\">
 								<div style=\"position: absolute; right: 0; top: 0;\"><a href=\"#\" onclick=\"showQuestions(2); return false;\"><img class=\"link\" src=\"/{$GLOBALS['CONFIG']['SITE']}/images/icons/small/close.png\" alt=\"\" /></a></div>
@@ -136,10 +110,22 @@ $content = "
 										<td>178.32.65.67</td>
 									</tr>
 								</table>
+								<div style=\"float:right; margin-top: 20px;\">
+									<a class=\"button classic\" style=\"width: 275px; height: 22px; float: right;\" href=\"/doc/publish\">
+										<img src=\"/on/images/arrow-right.png\" style=\"float: left;\">
+										<span style=\"display: block; padding-top: 3px;\">{$lang['domain_conf']}</span>
+									</a>
+								</div>
 							</div>
 							<div id=\"answer-3\" class=\"answer\">
 								<div style=\"position: absolute; right: 0; top: 0;\"><a href=\"#\" onclick=\"showQuestions(3); return false;\"><img class=\"link\" src=\"/{$GLOBALS['CONFIG']['SITE']}/images/icons/small/close.png\" alt=\"\" /></a></div>
 								<p>{$lang['dir']}</p>
+								<div style=\"float:right; margin-top: 20px;\">
+									<a class=\"button classic\" style=\"width: 275px; height: 22px; float: right;\" href=\"/doc/publish\">
+										<img src=\"/on/images/arrow-right.png\" style=\"float: left;\">
+										<span style=\"display: block; padding-top: 3px;\">{$lang['domain_conf']}</span>
+									</a>
+								</div>
 							</div>
 							<div id=\"answer-4\" class=\"answer\">
 								<div style=\"position: absolute; right: 0; top: 0;\"><a href=\"#\" onclick=\"showQuestions(4); return false;\"><img class=\"link\" src=\"/{$GLOBALS['CONFIG']['SITE']}/images/icons/small/close.png\" alt=\"\" /></a></div>
@@ -148,6 +134,12 @@ $content = "
 							<div id=\"answer-5\" class=\"answer\">
 								<div style=\"position: absolute; right: 0; top: 0;\"><a href=\"#\" onclick=\"showQuestions(5); return false;\"><img class=\"link\" src=\"/{$GLOBALS['CONFIG']['SITE']}/images/icons/small/close.png\" alt=\"\" /></a></div>
 								<p>{$lang['access']}</p>
+								<div style=\"float:right; margin-top: 20px;\">
+									<a class=\"button classic\" style=\"width: 275px; height: 22px; float: right;\" href=\"/doc/databases\">
+										<img src=\"/on/images/arrow-right.png\" style=\"float: left;\">
+										<span style=\"display: block; padding-top: 3px;\">{$lang['db_conf']}</span>
+									</a>
+								</div>
 							</div>
 							<div id=\"answer-6\" class=\"answer\">
 								<div style=\"position: absolute; right: 0; top: 0;\"><a href=\"#\" onclick=\"showQuestions(6); return false;\"><img class=\"link\" src=\"/{$GLOBALS['CONFIG']['SITE']}/images/icons/small/close.png\" alt=\"\" /></a></div>
@@ -170,10 +162,12 @@ $content = "
 										<td><a href=\"https://pma.olympe.in\">https://ppa.olympe.in</a></td>
 									</tr>
 								</table>
-							</div>
-							<div id=\"answer-7\" class=\"answer\">
-								<div style=\"position: absolute; right: 0; top: 0;\"><a href=\"#\" onclick=\"showQuestions(7); return false;\"><img class=\"link\" src=\"/{$GLOBALS['CONFIG']['SITE']}/images/icons/small/close.png\" alt=\"\" /></a></div>
-								<p>{$lang['statswhere']}</p>
+								<div style=\"float:right; margin-top: 20px;\">
+									<a class=\"button classic\" style=\"width: 275px; height: 22px; float: right;\" href=\"/doc/databases\">
+										<img src=\"/on/images/arrow-right.png\" style=\"float: left;\">
+										<span style=\"display: block; padding-top: 3px;\">{$lang['db_conf']}</span>
+									</a>
+								</div>
 							</div>
 							<div id=\"answer-8\" class=\"answer\">
 								<div style=\"position: absolute; right: 0; top: 0;\"><a href=\"#\" onclick=\"showQuestions(8); return false;\"><img class=\"link\" src=\"/{$GLOBALS['CONFIG']['SITE']}/images/icons/small/close.png\" alt=\"\" /></a></div>
