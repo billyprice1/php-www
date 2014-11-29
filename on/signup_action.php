8569c01053cac509fcbedffb7f9c8b27f812f5c3
Romain Perrone <romain.perrone@olympe.in>
2014-11-29 20:50:30 +0100

    check email (DNS lookup)

--- on/signup_action.php
+++ on/signup_action.php
@@ -24,6 +24,9 @@ if( isset($_POST['antispam']) && $_POST['antispam'] == $_SESSION['ANTISPAM'] && 
 
 		if( array_search($parts[1], $banned) !== false )
 			throw new SiteException('Invalid or missing arguments', 400, 'Parameter email is on a spammer domain');
+
+		if( in_array(gethostbyname($parts[1]), array(gethostbyname('ns1.olympe.in'), gethostbyname('ns2.olympe.in'))) )
+			throw new SiteException('Invalid or missing arguments', 400, 'Email does not exist');
 	
 		$result = api::send('registration/add', array('auth'=>'', 'email'=>$_POST['email']), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
 
