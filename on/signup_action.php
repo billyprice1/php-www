19fdacdd9a5866346f9ee8532d494117aa021702
Romain Perrone <romain.perrone@olympe.in>
2014-11-29 19:10:35 +0100

    check email domain dns (MX) record , signup

--- on/signup_action.php
+++ on/signup_action.php
@@ -24,6 +24,9 @@ if( isset($_POST['antispam']) && $_POST['antispam'] == $_SESSION['ANTISPAM'] && 
 
 		if( array_search($parts[1], $banned) !== false )
 			throw new SiteException('Invalid or missing arguments', 400, 'Parameter email is on a spammer domain');
+	
+		if( in_array(gethostbyname($parts[1]), array(gethostbyname("this_is_a_wrong_url.com"))) || !checkdnsrr($parts[1] , 'MX') )
+			throw new SiteException('Invalid or missing arguments', 400, 'Parameter email does not exist');
 
 		$result = api::send('registration/add', array('auth'=>'', 'email'=>$_POST['email']), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
 
