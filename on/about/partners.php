787540820e44400d3a9846ed4a2cf68373b45dca
Thomas <thomas.morain@olympe.in>
2014-12-03 12:06:51 +0100

    Partnership : not english yet

--- on/about/partners.php
+++ on/about/partners.php
@@ -6,6 +6,11 @@ if( !defined('PROPER_START') )
 	exit;
 }
 
+if($_SESSION['CONFIG']['LANGUAGE'] == 'EN') 
+	$partner_width = "500px";
+else 
+	$partner_width = "330px";
+	
 $content = "
 		<div class=\"head-light\">
 			<div class=\"container\">
@@ -13,18 +18,25 @@ $content = "
 			</div>
 		</div>	
 		<div class=\"content\">
-			<div style=\"float: left; text-align: justify; margin-right: 50px; width: 330px;\">
+			<div style=\"float: left; text-align: justify; margin-right: 50px; width: {$partner_width};\">
 				<a href=\"http://www.interxion.fr\" style=\"display: block; height: 120px;\"><img style=\"display: block; margin: 0 auto;\" src=\"/{$GLOBALS['CONFIG']['SITE']}/images/partners/interxion.png\" alt=\"\" /></a>
 				<p>{$lang['interxion']}</p>
 			</div>
-			<div style=\"float: left; text-align: justify; margin-right: 50px; width: 330px;\">
+			<div style=\"float: left; text-align: justify; margin-right: 50px; width: {$partner_width};\">
 				<a href=\"http://www.anotherservice.com\" style=\"display: block; height: 120px;\"><img style=\"padding-top: 0px; display: block; margin: 0 auto;\" src=\"/{$GLOBALS['CONFIG']['SITE']}/images/partners/as.png\" alt=\"\" /></a>
 				<p>{$lang['as']}</p>			
 			</div>
-			<div style=\"float: left; text-align: justify; width: 330px;\">
+";
+
+if($_SESSION['CONFIG']['LANGUAGE'] != 'EN') 
+	$content .= "
+			<div style=\"float: left; text-align: justify; width: {$partner_width};\">
 				<a href=\"{$lang['fa_link']}\" style=\"display: block; height: 100px; padding-top: 20px;\"><img style=\"padding-top: 0px; display: block; margin: 0 auto; width: 100%;\" src=\"/{$GLOBALS['CONFIG']['SITE']}/images/partners/{$lang['fa_img']}.png\" alt=\"\" /></a>
 				<p>{$lang['fa_text']}</p>			
 			</div>
+	";
+	
+$content .= "
 			<div class=\"clear\"></div><br />
 		</div>
 	</div>
