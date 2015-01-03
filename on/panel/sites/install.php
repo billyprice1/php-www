21d09bd3270e0a55cc22ee66827d42f257ef2f3b
Romain Perrone <ro.perrone1@gmail.com>
2015-01-03 11:24:06 +0100

    bugfix

--- on/panel/sites/install.php
+++ on/panel/sites/install.php
@@ -25,22 +25,12 @@ catch( Exception $e )
 	$_SESSION['MESSAGE']['TEXT']= $lang['error'];
 }
 
-$con = ftp_connect('ftp.olympe.in');
-$login = ftp_login($con, $site['name'], $_POST['pass']);
-
-if ( !is_array( ftp_nlist($con, ".") ) )
-	throw new SiteException('FTP connection to the server failed. Invalid password supplied', 400, 'connection failed');
-
-ftp_close($con);
-
-	
 $content = file_get_contents( 'https://fr.wordpress.org/wordpress-4.1-fr_FR.zip' );
 file_put_contents( 'ftp://'.$site['name'].':'.$_POST['pass'].'@ftp.olympe.in/file.zip', $content, 0, stream_context_create( array('ftp' => array('overwrite' => true)) ));
 
-if ( file_exists('ftp://'.$site['name'].':'.$_POST['pass'].'@ftp.olympe.in/file.zip') )
-{
-	$zip = new ZipArchive;
-	$res = $zip->open('ftp://'.$site['name'].':'.$_POST['pass'].'@ftp.olympe.in/file.zip');
+$zip = new ZipArchive;
+$res = $zip->open('ftp://'.$site['name'].':'.$_POST['pass'].'@ftp.olympe.in/file.zip');
+	
 	if ( $res  === TRUE) 
 	{
 		 // extract it to the path we determined above
@@ -54,11 +44,9 @@ if ( file_exists('ftp://'.$site['name'].':'.$_POST['pass'].'@ftp.olympe.in/file.
 		  header("Location: http://{$site['name']}.olympe.in/wordpress/wp-admin/setup-config.php?name={$database['name']}&server={$database['server']}");
 		  return;
 	} 
+	
 	else 
 	  throw new SiteException('Internal Error '.$res, 400, 'File can not be extracted');
-}
-else
-	throw new SiteException('Invalid argument', 400, 'File not uploaded');
 	
 function random($car) 
 {
