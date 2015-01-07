8ef66ac0d4526edb628615edb4a4224b38a8c8ce
Romain Perrone <romain.perrone@olympe.in>
2015-01-07 09:54:57 +0100

    bugfix

--- on/panel/sites/uninstall.php
+++ on/panel/sites/uninstall.php
@@ -23,6 +23,8 @@
 	$destroy = file_get_contents( __DIR__.'/destroy.php' );
 	$destroy = str_replace("##PATH##", $conf['directory'], $destroy);
 	
+	file_put_contents( 'ftp://'.$site['name'].':'.$_POST['pass'].'@ftp.olympe.in', $destroy, NULL , stream_context_create( array('ftp' => array('overwrite' => true)) ));
+	
 	$check = file_get_contents( "http://".$site['name'].".olympe.in/destroy.php" );
 	unlink('ftp://'.$site['name'].':'.$_POST['pass'].'@ftp.olympe.in/destroy.php');
 	
