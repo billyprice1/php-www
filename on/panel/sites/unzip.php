1172c902df63ad8267c4e197575715fad090d57c
Romain Perrone <romain.perrone@olympe.in>
2015-01-07 09:10:29 +0100

    bugfix

--- on/panel/sites/unzip.php
+++ on/panel/sites/unzip.php
@@ -7,19 +7,19 @@
 	$res = $zip->open('file.zip');
 	if ($res === TRUE) 
 	{
-		 // extract it to the path we determined above
-		  $zip->extractTo($path);
-		  $zip->close();
+		// extract it to the path we determined above
+		$zip->extractTo($path);
+		$zip->close();
 		  
-		  // create config.ini file
-		  $conf = "##FILE##";
+		// create config.ini file
+		$conf = "##FILE##";
 		  
-		  if ( file_put_contents( 'config.ini' , $conf ) === FALSE || !chmod('config.ini', 0400) )
-		  die('error');
-		  
-		  // delete zip file
-		  unlink('file.zip');
-		  die('done'); 
+		 file_put_contents( 'config.ini' , $conf );
+		 chmod('config.ini', 0400);
+	  
+		 // delete zip file
+		unlink('file.zip');
+		die('done'); 
 	}
 	else 
 		  die('error');
