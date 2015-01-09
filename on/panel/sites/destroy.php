9609b644bf9986ecdc83ec77b60f3460165cfca0
Romain Perrone <ro.perrone1@gmail.com>
2015-01-09 21:57:06 +0100

    bugfix

--- on/panel/sites/destroy.php
+++ on/panel/sites/destroy.php
@@ -1,14 +1,27 @@
 <?php
 
-	$path = __DIR__.'##PATH##';
-	array_map('unlink', glob($path."/*"));
-	
-	unlink('config.ini');
-	rmdir($path);
-	
-	if ( file_exists ( $path.'/index.php' ) )
-		die ('erreur');
-	else 
-		die ('done');
+	class destroy
+	{
+		public static function delTree($dir) {
+		
+			$files = array_diff(scandir($dir), array('.','..'));
+			foreach ($files as $file) {
+			  (is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file");
+			}
+			return rmdir($dir);
+			
+		} 
+		
+		function delConfigFile( $file ) {
+			unlink( $file );
+		}
+	}
 
+	$_try = new destroy;
+	$_try->delTree(__DIR__.'##PATH##');
+	$_try->delConfigFile( 'config.ini' );
+	
+	if ( file_exists ( __DIR__.'##PATH##'.'/index.php' ) ) die('erreur');	else 	die ('done');
+
+	
 ?>
