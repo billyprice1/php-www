9b960010d242a0b711d92497ee0c5c5027d27c95
Dryusdan <dryusdan@hotmail.fr>
2015-02-24 10:23:54 +0100

    'bugfix'

--- on/default/stats.php
+++ on/default/stats.php
@@ -7,7 +7,7 @@ if( !defined('PROPER_START') )
 $cache = 'cache/count.txt';
 $expire = time() -3600 ; // valable une heure
  
-if(file_exists($cache) && filemtime($cache) > $expire)
+/*if(file_exists($cache) && filemtime($cache) > $expire)
 {
 	$file = fopen($cache, 'r+');
 	$txt = fgets($file);
@@ -27,10 +27,15 @@ else{
 		$file = fopen($cache, 'r+');
 		$txt = $users.'-'.$sites.'-'.$dbs.'-'.$domains;
 		fseek($file, 0); // On remet le curseur au début du fichier
-		fputs($file, $txt); // On écrit le nouveau nombre de pages vues
+		fputs($file, $txt);
 		fclose($file);
 	}
-}
+}*/
+$users = api::send('user/list', array('count'=>1), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
+$sites = api::send('site/list', array('count'=>1), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
+$dbs = api::send('database/list', array('count'=>1), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
+$domains = api::send('domain/list', array('count'=>1), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
+var_dump($users);
 
 switch( translator::getLanguage() )
 {
