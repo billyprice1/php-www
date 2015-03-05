ef76df5e66c093fd3f1de3e9ad8d19e149c5da2d
Dryusdan <dryusdan@hotmail.fr>
2015-03-05 21:31:26 +0100

    number in quota

--- on/panel/messages.php
+++ on/panel/messages.php
@@ -103,7 +103,7 @@ $content .= "
 							<span class=\"help-block\">{$lang['quota_select']}</span>
 						</fieldset>
 						<fieldset>
-							<input class=\"auto req\" type=\"text\" style=\"width: 400px;\" name=\"max\" placeholder=\"{$lang['number']}\" />
+							<input class=\"auto req\" type=\"number\" style=\"width: 400px;\" name=\"max\" placeholder=\"{$lang['number']}\" />
 							<span class=\"help-block\">{$lang['number_help']}</span>
 						</fieldset>
 						<fieldset>
