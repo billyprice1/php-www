d55bb2df914fde59824951dd4a5c341b63155d24
Dryusdan <lechanony@B0214-02.lyc-sudloire-44.local>
2015-03-06 11:41:16 +0100

    reset file

--- on/default/stats.php
+++ on/default/stats.php
@@ -1,5 +1,4 @@
 <?php
-$timestamp_debut = microtime(true);
 if( !defined('PROPER_START') ) 
 { 
     header("HTTP/1.0 403 Forbidden"); 
@@ -10,12 +9,7 @@ $expire = time() -3600 ; //cache une heure à voir si je l'active entièrement d
 $file = fopen($cache, 'w+');
 $txt = fgets($file);
 $txt = explode('-', $txt);*/
-$users = api::send('user/list', array('count'=>1), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
-$sites = api::send('site/list', array('count'=>1), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
-$dbs = api::send('database/list', array('count'=>1), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
-$domains = api::send('domain/list', array('count'=>1), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
-
-/*if($_SERVER["HTTP_HOST"] == 'localhost' || $_SERVER["HTTP_HOST"] == '127.0.0.1' || $_SERVER["HTTP_HOST"] == 'local.olympe.in'){
+if($_SERVER["HTTP_HOST"] == 'localhost' || $_SERVER["HTTP_HOST"] == '127.0.0.1' || $_SERVER["HTTP_HOST"] == 'local.olympe.in'){
 	$users = api::send('user/list', array('count'=>1), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
 	$sites = api::send('site/list', array('count'=>1), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
 	$dbs = api::send('database/list', array('count'=>1), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
@@ -39,7 +33,7 @@ elseif($_SERVER["HTTP_HOST"] == 'www.dev.olympe.in'){
 	$sites['count'] = $get_result->{'sites'};
 	$dbs['count'] = $get_result->{'dbs'};
 	$domains['count'] = $get_result->{'domains'};
-}*/
+}
 /*var_dump($get_result);*/
 /*if(date('G:i') >= '17:30' && date('G:i') <= '22:00' || date('G:i') >= '6:00' && date('G:i') <= '9:00')
 {
@@ -95,13 +89,4 @@ $content = "
 
 echo $content;
 
- 
-// timestamp en millisecondes de la fin du script
-$timestamp_fin = microtime(true);
- 
-// différence en millisecondes entre le début et la fin
-$difference_ms = $timestamp_fin - $timestamp_debut;
- 
-// affichage du résultat
-echo 'Exécution du script : ' . $difference_ms . ' secondes.';
 ?>
