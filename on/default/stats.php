301fd48eca6c5cba9bf93e0d4fee9e3aff25d4f6
Dryusdan <dryusdan@hotmail.fr>
2015-03-25 21:29:48 +0100

    memcache test

--- on/default/stats.php
+++ on/default/stats.php
@@ -19,7 +19,7 @@ else
 {
 	$memcache = new Memcache;
 	$memcache->addServer('outdoor', 11211);
-	echo $memcache->getServerStatus('outdoor', 11211);
+	echo $memcache->getServerStatus('outdoor2', 11211);
 	/*$memcache->connect('memcache', 11211);
 	$get_result = $memcache->get('stats');
 	if(!$get_result){
