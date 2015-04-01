b50aa8dcde3c6969cb02301d54f6bd227b500271
Thomas <thomas.morain@olympe.in>
2015-04-01 22:51:21 +0200

    Lowercase username on login action #326

--- on/login_action.php
+++ on/login_action.php
@@ -7,13 +7,14 @@ if( !defined('PROPER_START') )
 }
 
 $_SERVER['HTTP_REFERER'] = str_replace('&erecovery', '', $_SERVER['HTTP_REFERER']);
+$username = strtolower($_POST['username']);
 
 if( isset($_POST['antispam']) && $_POST['antispam'] == $_SESSION['ANTISPAM'] )
 {
 	try
 	{
 		unset($_SESSION['ANTISPAM']);
-		$security->login($_POST['username'], $_POST['password'], ($_POST['remember'] == 'remember'));
+		$security->login($username, $_POST['password'], ($_POST['remember'] == 'remember'));
 	}
 	catch(Exception $e)
 	{
