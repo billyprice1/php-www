d61b1ec8c94a5341ccbc8a8d9ff270a21d1d9efe
Thomas <thomas.morain@olympe.in>
2015-05-24 23:36:43 +0200

    Bugfix (#000)

--- on/panel/sites/install.php
+++ on/panel/sites/install.php
@@ -64,7 +64,7 @@
 			throw new SiteException('Please remove one of your databases ', 400, 'quota reached');
 
 	$install_date = date("Y-m-d");
-	$db_name = $_GLOBALS['APP']['NAME']. ' - autoinstalled - $install_date'. ;
+	$db_name = $_GLOBALS['APP']['NAME']. " - autoinstalled - {$install_date}";
 			
 	$new = api::send('self/database/add', array('type'=>'mysql', 'desc'=> $_GLOBALS['APP']['NAME'], 'pass'=> $_GLOBALS['APP']['PASSWORD'] ));
 	$database = api::send( 'self/database/list', array( 'database' => $new['name'] ) )[0];
