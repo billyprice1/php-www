08a3387f091754000e2f327165437e89ca6d2592
Thomas <thomas.morain@olympe.in>
2015-08-16 15:33:01 +0200

    Ban website (test) #000

--- on/admin/sites/ban_action.php
+++ on/admin/sites/ban_action.php
@@ -26,8 +26,7 @@
 	
 	/* display banned page */
 	$htaccess = '404/.htaccess';
-	$font = '404/BebasNeue Regular.ttf';
-	$index = __DIR__.'/404/test.txt';
+	$index = __DIR__.'/404/index.html';
 	
 	/*
 	$index = file_get_contents( __DIR__.'/404/index.html' );
@@ -39,25 +38,12 @@
 	api::send('site/insert', array('site'=>$site_name, 'user'=>$banned_sites_user_id, 'pass'=>$new_password));
 	sleep(10);
 	
-	/*
 	$conn_id = ftp_connect("ftp.olympe.in") or die("Couldn't connect to $ftp_server");
 	ftp_login($conn_id, $site_name, $new_password)
 	ftp_pasv($conn_id, true);
-	ftp_put($conn_id, "test.txt", $index, FTP_ASCII)
+	ftp_put($conn_id, "index.html", $index, FTP_ASCII)
+	ftp_put($conn_id, ".htaccess", $htaccess, FTP_ASCII)
 	ftp_close($conn_id);
-	*/
-	
-	$connection = ssh2_connect('ftp.olympe.in', 22);
-	ssh2_auth_password( $connection, $site_name, $new_password );
-	/*ssh2_scp_send($connection, $htaccess, '/.htaccess', 0644);
-	ssh2_scp_send($connection, $font, '/BebasNeue Regular.ttf', 0644);
-	*/
-	if(ssh2_scp_send($connection, $index, '/test.txt', 0644))
-		echo 'OK';
-	else 
-		echo 'Not ok :(';
-	
-	
 	
 	/*
 	if( isset($_GET['redirect']) )
